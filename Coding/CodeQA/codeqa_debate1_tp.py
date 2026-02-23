2f32a0b605074b4dac14a32774ad0b65a5a2df63
MahaZainab <mzainab.msit16seecs@seecs.edu.pk>
2026-02-23 15:29:51 -0600

    Auto commit on save

--- Coding/CodeQA/codeqa_debate1_tp.py
+++ Coding/CodeQA/codeqa_debate1_tp.py
@@ -432,8 +432,8 @@ class ModelRunner:
             outputs = self.model.generate(
                 **inputs,
                 max_new_tokens=max_new_tokens,
-                do_sample=True,
-                temperature=0.3,
+                do_sample=False,
+                temperature=0.0,
                 pad_token_id=self.tokenizer.pad_token_id,
                 eos_token_id=self.tokenizer.eos_token_id,
                 stopping_criteria=stopping,
